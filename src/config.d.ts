ec9c42a981d756c97349300589d4ee31b12a9e20
Tomer Horowitz <tomerh@realcommerce.co.il>
2025-04-09 00:44:59 +0300

    chore: add config types for scraper credentials and bank configurations

---
+++ src/config.d.ts
@@ -0,0 +1,26 @@
+import type {InitConfig} from '@actual-app/api/@types/loot-core/server/main';
+import type {ScraperCredentials, CompanyTypes} from 'israeli-bank-scrapers';
+
+export type Config = {
+	banks: ConfigBanks;
+	actual: ConfigActual;
+};
+
+export type ConfigActual = {
+	init: InitConfig;
+	budget: ConfigActualBudget;
+};
+
+export type ConfigActualBudget = {
+	syncId: string;
+	password: string;
+};
+
+export type ConfigBanks = Partial<{
+	[key in CompanyTypes]: ConfigBank;
+}>;
+
+export type ConfigBank = ScraperCredentials & {
+	actualAccountId: string;
+	reconcile?: boolean;
+};
